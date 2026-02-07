a69ca50562b7aab4ba9c5740bcfb7e171e2e0520
4664robotics <4664robotics@gmail.com>
2026-02-07 09:43:41 -0600

    Cleaned up auto alignment and tweaked values

--- src/main/java/frc/robot/commands/swervedrive/auto/TargetAutoAlignCommand.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/TargetAutoAlignCommand.java
@@ -15,13 +15,37 @@ public class TargetAutoAlignCommand extends Command {
     public SwerveSubsystem drivebase;
     Limelight limelight = new Limelight("limelight-butler");
 
+    // == AUTO-ALIGNMENT CONFIGURATION ==
+
+    // defines which april tags are "left" and "right" april tags so the robot always knows what it's looking for
+    int[] leftTagIds = {1};
+    int[] rightTagIds = {2};
+
+    final double targetDistance = 15.925449429385568; // the coordinate length value that the robot is trying to reach
+    final double targetDistanceError = 0.15; // the amount of error that's allowed when trying to reach the target distance value
+    final double driveRubberBandingMultiplier = 1; // robot drive speed when rubberbanding
+    final double correctionSpeed = 0.5; // the speed for the robot to drive backwards if it gets too close to the april tags
+    
+    final double seekingRotationSpeed = 1.5; // the speed the robot rotates at when searching for april tags
+    final double rotationalRubberBandingMultiplier = 0.15; // rotational speed when rubberbanding
+    final double seekingError = 0.05; // the amount of error that's allowed when trying to align with the april tags
+    
+    // be careful with these ones
+    final double driveSpeedLimit = 0.9; // used when rubberbanding driving (measured in m/s)
+    final double rotationalSpeedLimit = 0.8; // used when rotationally rubberbanding, prevents the robot from going psycho (measured in degrees per second)
+    
+
+
+
+
+
+    // == DON'T TOUCH THESE ==
+
     // current stage of aligning, with 0 being rotating, and 1 being moving back and forth
     // when it equals 2, the robot completes alignment and stops moving
     int currentAligningStage = 0; 
 
-    // defines which april tags are on the left and the right to eliminate error
-    int[] leftTagIds = {1};
-    int[] rightTagIds = {2};
+
 
     public TargetAutoAlignCommand(SwerveSubsystem swerveSubsystem) {
         drivebase = swerveSubsystem;
@@ -90,9 +114,9 @@ public class TargetAutoAlignCommand extends Command {
         }
     }
 
-    public double getRubberBandingSpeed(double distanceFromCenter) {
-        if (distanceFromCenter > 0.15 || distanceFromCenter < -0.15) {
-            return clampCalc(distanceFromCenter * -0.15, -0.8, 0.8);
+    public double getRubberBandingSpeed(double distanceFromCenter, double multiplier, double limit) {
+        if (distanceFromCenter > seekingError || distanceFromCenter < seekingError * -1) {
+            return clampCalc(distanceFromCenter * multiplier * -1, limit * -1, limit);
         } else {
             return 0.0;
         }
@@ -113,41 +137,35 @@ public class TargetAutoAlignCommand extends Command {
                 drivebase.zeroGyro();
                 double middle = validAprilTags.getTagMidpoint();
 
-                drivebase.drive(new Translation2d(0, 0), getRubberBandingSpeed(middle), false);
+                drivebase.drive(new Translation2d(0, 0), getRubberBandingSpeed(middle, rotationalRubberBandingMultiplier, rotationalSpeedLimit), false);
 
                 // check if robot is aligned
                 if (isApproximatelyEqual(middle, 0, 0.15)) {
                     currentAligningStage = 1;
-                    System.out.println("Aligning stage 1 reached!");
                 }
             } else {
-                drivebase.drive(new Translation2d(0, 0), 2, false);
+                drivebase.drive(new Translation2d(0, 0), seekingRotationSpeed, false);
             }
 
         } else {
             TargetAprilTags validAprilTags = retrieveValidTargetTags(getAprilTags().get());
-            final double targetDistance = 8.556558354542982;
-            final double driveSpeed = 0.15;
+
 
             if (validAprilTags.isValid()) {
                 double coordinateDistance = validAprilTags.getTagDistance();
 
                 System.out.println(coordinateDistance);
 
-                if (isApproximatelyEqual(coordinateDistance, targetDistance, 0.15)) {
-                    System.out.println("Robot finished!");
+                if (isApproximatelyEqual(coordinateDistance, targetDistance, targetDistanceError)) {
                     currentAligningStage = 2;
-                } else if (coordinateDistance > targetDistance + 0.15) {
-                    System.out.println("AprilTags too close! Moving backwards.");
-                    drivebase.drive(new Translation2d(driveSpeed * -1, 0), 0.0, false);
+                } else if (coordinateDistance > targetDistance + targetDistanceError) {
+                    drivebase.drive(new Translation2d(getRubberBandingSpeed(coordinateDistance - targetDistance, driveRubberBandingMultiplier, driveSpeedLimit), 0), 0.0, false);
                 } else {
-                    System.out.println("AprilTags too far! Moving forwards.");
-                    drivebase.drive(new Translation2d(driveSpeed, 0), 0.0, false);
+                    drivebase.drive(new Translation2d(getRubberBandingSpeed(coordinateDistance - targetDistance, driveRubberBandingMultiplier, driveSpeedLimit), 0), 0.0, false);
                 }
                 
             } else {
-                System.out.println("Tags invalid! Moving backwards.");
-                drivebase.drive(new Translation2d(driveSpeed * -1, 0), 0.0, false);
+                drivebase.drive(new Translation2d(correctionSpeed * -1, 0), 0.0, false);
             }
             
         }
@@ -157,6 +175,7 @@ public class TargetAutoAlignCommand extends Command {
     @Override
     public boolean isFinished() {
         if (currentAligningStage == 2) {
+            System.out.println("Aligning complete!");
             return true;
         }
         return false;
