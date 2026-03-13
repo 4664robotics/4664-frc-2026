384233034684b155606ba662edd8c03f43db60f9
4664robotics <4664robotics@gmail.com>
2026-03-13 15:51:14 -0500

    Wrote motor controller commands

---
+++ src/main/java/frc/robot/commands/swervedrive/drivebase/IntakeCommand.java
@@ -0,0 +1,31 @@
+package frc.robot.commands.swervedrive.drivebase;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class IntakeCommand extends Command {
+
+    SparkMax smg = new SparkMax(0, MotorType.kBrushless);
+
+    @Override
+    public void initialize(){
+
+    }
+
+    @Override
+    public void execute(){
+        smg.set(0.1);
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        smg.set(0);
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+}
